1b1dd26662068d71130bc6c79854ff7d942ea68f
Stims29 <73816508+Stims29@users.noreply.github.com>
2020-12-02 10:11:07 +0100

    Update AutoCreateUsers.py

--- AutoCreateUsers.py
+++ AutoCreateUsers.py
@@ -5,18 +5,18 @@ import datetime
 import os
 import ldap
 
-# Initialiser une nouvelle connection ldap pour modifier l'Active Directory avec le compte Administrateur
+# Initialiser une nouvelle connexion ldap pour modifier l'Active Directory avec le compte Administrateur
 conn = ldap.initialize('ldap://127.0.0.1')
 conn.protocol_version = 3
 conn.set_option(ldap.OPT_REFERRALS, 0)
 conn.simple_bind_s('Administrateur@ledomaine.com','password')
 
-# Précise sur quel domaine la connection s'effectue ainsi que l'OU et le groupe        
+# Préciser sur quel domaine la connection s'effectue ainsi que l'OU et le groupe        
 domain_controller = 'DC=ledomaine,DC=com'
 users_ou = 'OU=All,OU=Domain_Users,{}'.format(domain_controller)
 groups_ou = 'OU=Domain_Users_Groups,{}'.format(domain_controller)
 
-# Fonction qui crée le username(pour ouvrir un session sur le domaine), la fonction de l'utilisateur dans l'entreprise, le prénom et le nom complet.
+# Fonction qui crée le username(pour ouvrir une session sur le domaine), la fonction de l'utilisateur dans l'entreprise, le prénom et le nom complet.
 def create_user(username, employee_id, display_name, active=False):
     """
     Créé un nouvel utilisateur dans l'AD
@@ -70,14 +70,14 @@ file = open('users.txt', 'rt')
 
 for line in file:
     # La méthode strip() supprime tous les caractères à droite et à gauche de la chaîne de caractères.
-    # La fonction split() découpe une chaîne de caractères suivant les espaces qu'elle contient.
+    # La fonction split() découpe une chaîne de caractères suivant les espaces qu'elle contient. Les paramètres sont délimités par la ",".
     users_paramaters_list = line.strip().split(",")
     # On déclare un nouveau dictionnaire contenant les paramètres utilisateurs
     users_paramaters = {}
     users_paramaters['username'] = users_paramaters_list[0]
     users_paramaters['employee_id'] = users_paramaters_list[1]
     users_paramaters['display_name'] = users_paramaters_list[2]
-    # On appelle la fonction create_user pour associer les paramètres contenus dans le fichier texte 
+    # On appel la fonction create_user pour associer les paramètres contenus dans le fichier texte 
     create_user(users_paramaters['username'],users_paramaters['employee_id'],users_paramaters['display_name'],active=True)
 file.close()    
 
